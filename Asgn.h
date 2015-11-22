26d496072bcd1bbef6044e9e6a9a9fa165cc25dd
astrohoff <astrohoff@ufl.edu>
2015-11-22 14:02:22 -0500

    Create Asgn.h

---
+++ Asgn.h
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+class Asgn{
+	private:
+		std::vector<std::string> students; // students in course
+		std::vector<int> grades; // indices match students; = -1 if no grade
+		std::string name;
+		std::string type;
+		int points;
+		std::string duedate; // *Can this be changed to time_t for sorting?*
+		std::vector<int> getGrades();
+	public:
+		Asgn(std::string nm, std::string tp, int pt, std::string dt, std::vector<std::string> st);
+		// create other constructors for when not creating snew assignment?
+		void gradeAsgn(std::string nm); // enter grade for all students
+		void changeGrade(std::string nm, std::string stud); // enter grade for 1 student
+		void studentViewGrades(int stud_index);
+		void viewGrades();
+		std::string getName();
+}; 
