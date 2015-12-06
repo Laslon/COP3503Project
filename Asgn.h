623b83d043594706bece4030b5c62123c8c4e0e1
johnaloomis <johnaloomis@ufl.edu>
2015-12-06 18:29:26 -0500

    Update Asgn.h

--- Asgn.h
+++ Asgn.h
@@ -8,7 +8,7 @@ class Asgn{
 		std::string type;
 		int points;
 		std::string duedate;
-		std::vector<int> getGrades();
+		void pushGrade(int grade);
 	public:
 		Asgn(std::string nm, std::string typ, int pt, std::string dt, std::vector<std::string> st);
 		// create other constructors for when not creating snew assignment?
@@ -17,5 +17,5 @@ class Asgn{
 		void studentViewGrades(int stud_index);
 		void viewGrades();
 		std::string getName();
-		std::string getType();
+		
 }; 
