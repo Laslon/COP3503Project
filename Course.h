25114940a65b3a32f4899cd1ed61edfce98d3fc1
astrohoff <astrohoff@ufl.edu>
2015-12-06 11:45:14 -0500

    Update Course.h

--- Course.h
+++ Course.h
@@ -6,23 +6,24 @@ class Course{
 		std::string course_ID;
 		std::string course_name;
 		int credit_hours;
-		std::vector<std::string> students; // extract from txt
+		int weights[6];
+		std::vector<std::string> students;
 		std::vector<Asgn> asgns;
-		std::vector<Asgn> getAsgns();
+		void getInfo();
 		std::vector<std::string> getStudents();
+		std::vector<Asgn> getAsgns();
 		int getStudentIndex(std::string stud);
-		// need weightage field
 	public:
-		Course(std::string id, std::string nm, int hrs);
+		Course(std::string id);
+		void testPrint();
 		bool checkStudent(std::string stud);
 		float studentGrade(std::string stud);
 		void studentViewGrades(std::string stud);
 		void viewGrades(void);
 		void enterGrades(std::string nm);
 		void changeGrade(std::string nm, std::string stud);
-		void createAsgn(std::string nm, int pts, std::string dln, std::string tp); // should deadline be type Time/Date?
+		void createAsgn(std::string nm, int pts, std::string dln, std::string tp);
 		void deleteAsgn(std::string nm);
 		void dataMaintenance(void); // updates course grades for students and course average for teacher
 																		// when grades are added/changed
 };
-// create file i/o for grades: course_id, assignments, grades
